5a69456af325ea24802685c2e24ed40241d0eb8d
Mr. Aman Prajapati <180110032+Aman113Prajapati@users.noreply.github.com>
2026-05-01 09:12:14 +0530

    Update Footer.jsx

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <footer className='bg-primary text-light text-center p-2'>
-        © 2026 MyNewsGuide 
+    <footer className='bg-secondary text-light text-center p-2'>
+        © 2026 MyNewsApp created by 113114 
     </footer>
   )
 }
